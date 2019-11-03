d266de36e304d36e691a67e8e45919821c102c56
Juan Barroso <juan.barroso.025@gmail.com>
2019-11-03 22:15:30 +0000

    Sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 let gulp = require('gulp');
 let concatCss = require('gulp-concat-css');
 let cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
+
 
 gulp.task('concatCss', function() {
   return gulp
@@ -14,4 +16,12 @@ gulp.task('minifyCss', () => {
     .src('src/styles/**/*.css')
     .pipe(cleanCSS({ compatibility: 'ie8' }))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('sourcesMaps', function() {
+  gulp.src('src/styles/**/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(concatCss('super.css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/css'));
 });
